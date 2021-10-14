67f1182470d3ff2e609b1214795ad0ead38a7c7e
Tumworobere <annaht.annah@gmail.com>
2021-10-14 15:38:37 +0300

    Add responsive popup in mobile version

--- js/modal.js
+++ js/modal.js
@@ -6,6 +6,7 @@ const projectList = {
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
     liveServerbtn: 'https://tumworobere.github.io/Portfolio/',
     sourceCode: 'https://github.com/Tumworobere/Portfolio',
+
   },
   secondProject: {
     popupHeading: 'Multi-Post Stories 2',
@@ -23,6 +24,7 @@ const projectList = {
     liveServerbtn: 'https://tumworobere.github.io/Portfolio/',
     sourceCode: 'https://github.com/Tumworobere/Portfolio',
   },
+
   fourthProject: {
     popupHeading: 'Multi-Post Stories 4',
     languages: ['html', 'Bootstrap', 'Ruby on Rails'],
@@ -31,6 +33,7 @@ const projectList = {
     liveServerbtn: 'https://tumworobere.github.io/Portfolio/',
     sourceCode: 'https://github.com/Tumworobere/Portfolio',
   },
+
   fifthProject: {
     popupHeading: 'Multi-Post Stories 5',
     languages: ['html', 'Bootstrap', 'Ruby on Rails'],
@@ -39,6 +42,7 @@ const projectList = {
     liveServerbtn: 'https://tumworobere.github.io/Portfolio/',
     sourceCode: 'https://github.com/Tumworobere/Portfolio',
   },
+
   sixthProject: {
     popupHeading: 'Multi-Post Stories 6',
     languages: ['html', 'Bootstrap', 'Ruby on Rails'],
@@ -48,6 +52,7 @@ const projectList = {
     sourceCode: 'https://github.com/Tumworobere/Portfolio',
   },
 };
+
 const projects = [
   projectList.firstProject,
   projectList.secondProject,
@@ -56,6 +61,7 @@ const projects = [
   projectList.fifthProject,
   projectList.sixthProject,
 ];
+
 function modal(number) {
   const modalSection = document.querySelector('#modal');
   modalSection.style.display = 'flex';
@@ -74,10 +80,12 @@ function modal(number) {
     list.textContent = projects[number].languages[i];
     modalList.appendChild(list);
   }
+
   modalDesc.innerHTML = projects[number].description;
   document.querySelectorAll('.modal-btn > a')[0].href = projects[number].liveServerbtn;
   document.querySelectorAll('.modal-btn > a')[1].href = projects[number].sourceCode;
 }
+
 function closeModal() {
   document.querySelector('#modal').style.display = 'none';
   document.querySelector('*').style.overflow = 'auto';
