b416b56da6330a172bef030ec9b69f2f0d174243
Tumworobere <annaht.annah@gmail.com>
2021-10-14 05:29:55 +0300

    Fix linter errors

--- js/modal.js
+++ js/modal.js
@@ -62,7 +62,9 @@ const projects = [
   projectList.sixthProject,
 ];
 
+// eslint-disable-next-line no-unused-vars
 function modal(number) {
+  const modalSection = document.querySelector('#modal');
   modalSection.style.display = 'flex';
   document.querySelector('#portfolio').classList.add('activeblur');
   document.querySelector('.about').classList.add('activeblur');
@@ -85,9 +87,16 @@ function modal(number) {
   document.querySelectorAll('.modal-btn > a')[1].href = projects[number].sourceCode;
 }
 
+// eslint-disable-next-line no-unused-vars
 function closeModal() {
   document.querySelector('#modal').style.display = 'none';
   document.querySelector('*').style.overflow = 'auto';
   document.querySelector('#portfolio').classList.remove('activeblur');
   document.querySelector('.about').classList.remove('activeblur');
 }
+
+// function toggle() {
+// const closed = document.querySelector('#close-btn');
+// closed.classList.toggle('hidden');
+// }
+// toggle();
